9ad55b1a338fdb3206f7fbdbfc247309f41342d3
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-01 11:02:11 +0100

    empty action made for homepage

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,2 +1,6 @@
 class TestController < ApplicationController
+  def home
+    # This action is intentionally left empty.
+    # It serves as a placeholder for the home page of the test application.
+  end
 end
