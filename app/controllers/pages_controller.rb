b0b22551537ae0f39de0c0021f5064d3263c40d0
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-01 11:17:31 +0100

    pages controller formed with route added to home

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def home
+    # This action is intentionally left empty.
+    # It serves as a placeholder for the home page of the application.
+  end
+end
